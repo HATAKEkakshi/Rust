2888efa0e10d4a08731dbd390016a8428c868a50
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-03-09 15:37:09 +0530

    leaned about functions

--- function/src/main.rs
+++ function/src/main.rs
@@ -3,6 +3,9 @@ fn main() {
     function();
     singleparameter(323);
     secondfunction(42,"hemant");
+    ex();
+    let xy=returnfunction(32,32);
+    println!("the value return is -{}",xy);
 }
 
 fn function(){
@@ -13,4 +16,14 @@ fn singleparameter(x : i32){
 }
 fn secondfunction(x : i32 ,y : &str){
     println!("the value for x is -{} , the value for y is - {}",x,y);
+}
+fn ex(){
+    let y = {
+        let x=9;
+        x+1
+    };
+    println!("The value for y is - {}",y);
+}
+fn returnfunction(x:i32,y:i32) -> i32 {
+    x+y
 }
