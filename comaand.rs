e35d5980476d0fc23276300e35b1c1339be5a4bc
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-03-09 16:51:21 +0530

    changes

--- comaand.rs
+++ comaand.rs
@@ -3,4 +3,7 @@ fn main(){
     /* for creating new package = cargo new nameofthepackage*/
    /*for manual complining rustc filename then run ./nameofthefile  */ 
     //for printing tuple ,array use default formater println!("{:?}",nameofarroayortupe);
+    /*  indexing rule
+    array =nameofarray[indexnumber]
+    tuple =nmaeothetuple.indexnumber*/
 }
--- datatypes/src/main.rs
+++ datatypes/src/main.rs
@@ -1,10 +1,18 @@
+use std::io;
 fn main() {
     println!("Data types in rust!");
     test(356);
-    let tup=[32,64,45];
+    let mut tup: (i32,u8,f64)=(32,64,45.9);
     println!("{:?}",tup);
+    println!("{}",tup.2);
+    tup.0 = 23;
+    println!("{:?}",tup);
+    let mut input =String::new();
+    println!("yo say something");
+
 
 }
 fn test(x:i128){
     println!("{}",x);
 }
+
