759e619dc1e2fc435d338c13885f5b8c27b81ee4
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-03-09 13:26:30 +0530

    first commit

---
+++ comaand.rs
@@ -0,0 +1,4 @@
+fn main(){
+    println!("These are Basic command for rust command ");
+    /* for creating new package = cargo new nameofthepackage*/
+}
---
+++ hello.rs
@@ -0,0 +1,5 @@
+fn main(){
+    println!("hello world!");
+    println!("this is rust code be ready for the fun");
+    println!("so i started learning it today");
+}
