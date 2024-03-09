e4f7c310d060c683c819599942117dd0bf43166f
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-03-09 15:10:41 +0530

    fundtion learned

--- function/src/main.rs
+++ function/src/main.rs
@@ -1,8 +1,16 @@
 fn main() {
     println!("Hello, world!");
-    function()
+    function();
+    singleparameter(323);
+    secondfunction(42,"hemant");
 }
 
 fn function(){
     println!("This is my example for function");
+}
+fn singleparameter(x : i32){
+    println!("the value for x is - {}",x);
+}
+fn secondfunction(x : i32 ,y : &str){
+    println!("the value for x is -{} , the value for y is - {}",x,y);
 }
