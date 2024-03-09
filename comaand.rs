9e9bccb390d36930d8f88f4e29bf6b4481ad119b
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-03-09 16:34:45 +0530

    array and tuple command added

--- comaand.rs
+++ comaand.rs
@@ -2,4 +2,5 @@ fn main(){
     println!("These are Basic command for rust command ");
     /* for creating new package = cargo new nameofthepackage*/
    /*for manual complining rustc filename then run ./nameofthefile  */ 
+    //for printing tuple ,array use default formater println!("{:?}",nameofarroayortupe);
 }
--- datatypes/src/main.rs
+++ datatypes/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
     println!("Data types in rust!");
     test(356);
+    let tup=[32,64,45];
+    println!("{:?}",tup);
+
 }
 fn test(x:i128){
     println!("{}",x);
